c6d0dbf73913e1ff905f1db163e3292e58e6750a
Muhammad Asim <mazammasim@gmail.com>
2026-08-12 14:51:42 +0500

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> rows[9];
+        unordered_set<char> cols[9];
+        unordered_set<char> boxes[9];
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.') continue;
+
+                char ind = (i/3)*3 + (j/3);
+                char num = board[i][j];
+                if(rows[i].count(num) || 
+                   cols[j].count(num) ||
+                   boxes[ind].count(num)){
+                    return false;
+                   }
+
+                rows[i].insert(num);
+                cols[j].insert(num);
+                boxes[ind].insert(num);
+            }
+        }
+        return true;
+    }
+};
