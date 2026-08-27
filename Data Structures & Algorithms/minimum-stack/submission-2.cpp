2781be13f6810d783aea5f51e315aad6da005e92
Muhammad Asim <mazammasim@gmail.com>
2026-08-27 17:27:25 +0500

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    vector<int> stack;
+    vector<int> minStack;
+
+    void push(int val) {
+        stack.push_back(val);
+
+        if (minStack.empty() || val <= minStack.back())
+            minStack.push_back(val);
+    }
+
+    void pop() {
+        if (stack.empty())
+            return;
+
+        if (stack.back() == minStack.back())
+            minStack.pop_back();
+
+        stack.pop_back();
+    }
+
+    int top() {
+        return stack.empty() ? -1 : stack.back();
+    }
+
+    int getMin() {
+        return minStack.empty() ? -1 : minStack.back();
+    }
+};
